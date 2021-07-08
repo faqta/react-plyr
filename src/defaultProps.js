ac9a0fad76ac5ca4fdd6469ee1f33c1976d8e466
Pim Knops <pimknops@gmail.com>
2021-07-08 15:32:07 +0200

    Update Plyr to 3.6.8

--- src/defaultProps.js
+++ src/defaultProps.js
@@ -52,7 +52,7 @@ const defaultProps = {
   // Sprite (for icons)
   loadSprite: true,
   iconPrefix: 'plyr',
-  iconUrl: 'https://cdn.plyr.io/3.5.6/plyr.svg',
+  iconUrl: 'https://cdn.plyr.io/3.6.8/plyr.svg',
 
   // Blank video (used to prevent errors on source change)
   blankVideo: 'https://cdn.plyr.io/static/blank.mp4',
